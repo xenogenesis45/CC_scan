d22c50b24cc5954e2e225a57a90159b95d936f78
xenogenesis45 <xenogenesis45@gmail.com>
2021-02-16 10:14:37 -0500

    ejemplo

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+
+
+import 'bascode.dart';
+
+// void main(){
+//   runApp(
+//     MaterialApp(
+//       home: MyApp(),
+//     )
+//   );
+// }
+
+// class MyApp extends StatefulWidget {
+//   @override
+//   _MyAppState createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   Widget build(BuildContext context) {
+    
+//     return BarCode();
+    
+//   }
+// }
+
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+      //oculta el debug de la parte superio
+      debugShowCheckedModeBanner: false,
+      title: 'QR Reader',
+      initialRoute: 'home',
+      routes: {
+        'home': (BuildContext context) => BarCode()
+      },
+      //Cambia color de toda la aplicacion
+      theme: ThemeData(
+        primaryColor: Colors.purple
+      ), 
+    );
+  }
+}
+
+
